43c870b2dd5980527afe3761ec2a549b7bbea241
LosStreak404 <devoperr404@gmail.com>
2023-06-05 17:40:25 +0300

    ThirdCommit

--- StonePaper.py
+++ StonePaper.py
@@ -1,1 +1,16 @@
-print("Ok")
+from typing import Optional, Tuple, Union
+import customtkinter as CTk
+
+class Application(CTk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x400")
+        self.resizable(False, False)
+
+
+
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
