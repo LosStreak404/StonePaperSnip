69c7f2ed22844cc42186435186ddb7749110d976
LosStreak404 <devoperr404@gmail.com>
2023-06-05 18:00:36 +0300

    added message frame and message entry

--- StonePaper.py
+++ StonePaper.py
@@ -7,6 +7,19 @@ class Application(CTk.CTk):
         self.geometry("400x400")
         self.resizable(False, False)
 
+        self.massage_frame = CTk.CTkFrame(master=self, 
+                                          width=100, 
+                                          height=30, 
+                                          corner_radius=3,
+                                          fg_color="transparent")
+        self.massage_frame.grid(row=1, 
+                                column=0, 
+                                sticky="nsew", 
+                                padx=10, pady=10)
+        self.message_entry = CTk.CTkEntry(master=self.message_frame,
+                                          width=90,
+                                          height=25,
+                                          corner_radius=5) 
 
 
 
