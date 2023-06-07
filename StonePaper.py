e154223f1702d165050e17bc63055cde132df9d9
devoperr404 <devoperr404@gmail.com>
2023-06-08 00:33:41 +0300

    Small changes

--- StonePaper.py
+++ StonePaper.py
@@ -7,20 +7,27 @@ class Application(CTk.CTk):
         self.geometry("400x400")
         self.resizable(False, False)
 
-        self.massage_frame = CTk.CTkFrame(master=self, 
-                                          width=100, 
+        self.message_frame = CTk.CTkFrame(master=self, 
+                                          width=300, 
                                           height=30, 
                                           corner_radius=3,
                                           fg_color="transparent")
-        self.massage_frame.grid(row=1, 
+        
+        self.message_frame.grid(row=1, 
                                 column=0, 
                                 sticky="nsew", 
                                 padx=10, pady=10)
+        
         self.message_entry = CTk.CTkEntry(master=self.message_frame,
-                                          width=90,
+                                          width=370,
                                           height=25,
-                                          corner_radius=5) 
+                                          corner_radius=5,
+                                          fg_color="transparent") 
 
+        self.message_entry.grid(row=1, 
+                                column=0,
+                                sticky="nsew",
+                                padx=5, pady=5)
 
 
 
